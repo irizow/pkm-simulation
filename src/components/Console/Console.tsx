b19844f565d8ed946e02ce56b0ca8afdc3002afb
irizow <irisrossell@gmail.com>
2026-02-04 21:02:51 +0100

    dummy shell styles

--- src/components/Console/Console.tsx
+++ src/components/Console/Console.tsx
@@ -4,7 +4,15 @@ import styles from "./console.module.css";
 export default function Console({ children }: { children: React.ReactNode }) {
   return (
     <div className={styles.console}>
+      <div className={`${styles.handle} ${styles.left}`}>
+        <div className={`${styles.top_button}`}></div>
+        <div className={styles.round_shape}></div>
+      </div>
       <div className={styles.screen}>{children}</div>
+      <div className={`${styles.handle} ${styles.right}`}>
+        <div className={`${styles.top_button}`}></div>
+        <div className={styles.round_shape}></div>
+      </div>
     </div>
   );
 }
