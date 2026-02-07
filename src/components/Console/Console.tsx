150c3c1c139ac0e71e9698fbd373d997c71e571a
irizow <irisrossell@gmail.com>
2026-02-07 19:31:13 +0100

    create basic buttons

--- src/components/Console/Console.tsx
+++ src/components/Console/Console.tsx
@@ -5,11 +5,21 @@ export default function Console({ children }: { children: React.ReactNode }) {
   return (
     <div className={styles.console}>
       <div className={`${styles.handle} ${styles.left}`}>
+        <div className={`${styles.controller}`}>
+          <button className={`${styles.arrow} ${styles.left}`}></button>
+          <button className={`${styles.arrow} ${styles.up}`}></button>
+          <button className={`${styles.arrow} ${styles.right}`}></button>
+          <button className={`${styles.arrow} ${styles.down}`}></button>
+        </div>
         <div className={`${styles.top_button}`}></div>
         <div className={styles.round_shape}></div>
       </div>
       <div className={styles.screen}>{children}</div>
       <div className={`${styles.handle} ${styles.right}`}>
+        <div className={styles.buttonbox}>
+          <button className={styles.a}></button>
+          <button className={styles.b}></button>
+        </div>
         <div className={`${styles.top_button}`}></div>
         <div className={styles.round_shape}></div>
       </div>
