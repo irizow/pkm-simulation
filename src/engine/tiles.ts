ad5292dfa63ac452ea3668e16e5f003a0c217bb7
irizow <irisrossell@gmail.com>
2026-02-22 15:33:56 +0100

    change some tiles

--- src/engine/tiles.ts
+++ src/engine/tiles.ts
@@ -64,11 +64,58 @@ export const tiles = [
     src: "/pkm-simulation/tiles/house-floor.png",
     collides: false,
   },
+  {
+    name: "sand",
+    src: "/pkm-simulation/tiles/sand.png",
+    collides: false,
+  },
+  {
+    name: "sea",
+    src: "/pkm-simulation/tiles/sea.png",
+    collides: false,
+  },
+  {
+    name: "sea-edge",
+    src: "/pkm-simulation/tiles/sea-edge.png",
+    collides: true,
+  },
+  {
+    name: "rail",
+    src: "/pkm-simulation/tiles/rail.png",
+    collides: false,
+  },
+  {
+    name: "rail-sand",
+    src: "/pkm-simulation/tiles/rail-sand.png",
+    collides: false,
+  },
+  {
+    name: "rail-grass",
+    src: "/pkm-simulation/tiles/rail-grass.png",
+    collides: false,
+  },
+  {
+    name: "street-left-edge",
+    src: "/pkm-simulation/tiles/street-left-edge.png",
+    collides: false,
+  },
+  {
+    name: "street-right-edge",
+    src: "/pkm-simulation/tiles/street-right-edge.png",
+    collides: false,
+  },
+  {
+    name: "street-grass-corner-left",
+    src: "/pkm-simulation/tiles/street-grass-corner-left.png",
+    collides: false,
+  },
+  {
+    name: "street-grass-corner-right",
+    src: "/pkm-simulation/tiles/street-grass-corner-right.png",
+    collides: false,
+  },
 ];
 
-// -------------------------------
-// IMAGE LOADING
-// -------------------------------
 export const tileImages: HTMLImageElement[] = tiles.map((t) => {
   const img = new Image();
   img.src = t.src;
@@ -77,25 +124,26 @@ export const tileImages: HTMLImageElement[] = tiles.map((t) => {
 
 //0: grass, 1: water, 2: water-left-edge, 3: water-right-edge, 4: water-bottom-edge,
 // 5: water-top-edge, 6: path, 7: path-left-edge, 8: path-right-edge, 9: path-bottom-edge,
-// 10: path-top-edge, 11: tall-tree, 12: house-floor
+// 10: path-top-edge, 11: tall-tree, 12: house-floor, 13: sand, 14: sea, 15: sea edge, 16: rail, 17: rail-sand, 18: rail-grass
+// 19: street left edge, 20: street right edge, 21: street grass corner left, 22: street grass corner right
 
 export const tileMapTown = [
-  [0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1],
-  [0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1],
-  [0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4],
-  [0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 7, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [10, 6, 6, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [0, 19, 20, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1],
+  [0, 19, 20, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1],
+  [0, 19, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4],
+  [0, 19, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 19, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 19, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 19, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 19, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 19, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [18, 21, 22, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18],
+  [16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16],
+  [17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17],
+  [13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13],
+  [15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15],
+  [14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14],
+  [14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14],
 ];
 
 export const tileMapMyHouse = [
